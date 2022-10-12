f7339b8bc8ec826260b10ea3811498f715a8fba3
nytah <onwubuyabenita@gmail.com>
2022-10-12 09:13:11 +0100

    corrected code

--- 0-bubble_sort.c
+++ 0-bubble_sort.c
@@ -10,7 +10,7 @@
  */
 void bubble_sort(int *array, size_t size)
 {
-    size_t i, j, n, new_n;
+    size_t i, n, new_n;
     int swap;
 
     if (array == NULL || size < 2)
@@ -21,15 +21,13 @@ void bubble_sort(int *array, size_t size)
     {
         new_n = 0;
         for (i = 0; i < n; i++){
-            for (j = 0; j < n; j++ ) {
-                if (array[0] > array[j+1]) {
-                    //swap numbers
-                    swap = array[j];
-                    array[j] = array[j+1];
-                    array[j+1] = swap;
-                    new_n = i + 1;
-                    print_array(array, size);
-                }
+            if (array[i] > array[i+1]) {
+                //swap numbers
+                swap = array[i];
+                array[i] = array[i+1];
+                array[i+1] = swap;
+                new_n = i + 1;
+                print_array(array, size);
             }
         }
         n = new_n;
