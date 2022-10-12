438465ab5449e16874d45bf7c06445efcad7c40e
nytah <onwubuyabenita@gmail.com>
2022-10-12 09:28:06 +0100

    fixed

--- 0-bubble_sort.c
+++ 0-bubble_sort.c
@@ -17,20 +17,21 @@ void bubble_sort(int *array, size_t size)
     return;
 
     n = size;
-    while (n > 0)
-    {
-        new_n = 0;
-        for (i = 0; i < n; i++){
-            if (array[i] > array[i+1])
-            {
+	while (n > 0)
+	{
+		new_n = 0;
+		for (i = 0; i < n - 1; i++)
+		{
+			if (array[i] > array[i + 1])
+			{
                 //swap numbers
-                swap = array[i];
-                array[i] = array[i+1];
-                array[i+1] = swap;
-                new_n = i + 1;
-                print_array(array, size);
-            }
-        }
-        n = new_n;
-    }
+				swap = array[i];
+				array[i] = array[i + 1];
+				array[i + 1] = swap;
+				new_n = i + 1;
+				print_array(array, size);
+			}
+		}
+		n = new_n;
+	}
 }
